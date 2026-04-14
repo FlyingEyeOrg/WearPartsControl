5bb3ccec278bc9767cac3354b68ef826cdc0530b
UnknowName <13604461032@163.com>
2026-04-14 19:15:12 +0800

    删除不需要的资源。

--- src/WearPartsControl/ViewModels/MainWindowViewModel.cs
+++ src/WearPartsControl/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,26 @@
-
 using WearPartsControl.ApplicationServices.Localization;
 
 namespace WearPartsControl.ViewModels
 {
     public class MainWindowViewModel
     {
+        private TabPageViewModel? _selectedTab;
+
         public string Title { get; set; }
+
+        public TabPageViewModel? SelectedTab
+        {
+            get => _selectedTab;
+            set
+            {
+                if (_selectedTab == value)
+                {
+                    return;
+                }
+
+                _selectedTab = value;
+            }
+        }
 
         public MainWindowViewModel(ILocalizationService localizationService)
         {
---
+++ src/WearPartsControl/ViewModels/TabPageViewModel.cs
@@ -0,0 +1,9 @@
+namespace WearPartsControl.ViewModels
+{
+    public class TabPageViewModel
+    {
+        public string Header { get; set; } = string.Empty;
+
+        public object? Content { get; set; }
+    }
+}
