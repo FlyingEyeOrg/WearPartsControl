df31c86e7717ec680b9a346ffdc93fa637e7a070
UnknowName <13604461032@163.com>
2026-04-14 21:27:40 +0800

    完善用户tab控件。

--- src/WearPartsControl/UserControls/UserTabControl.xaml.cs
+++ src/WearPartsControl/UserControls/UserTabControl.xaml.cs
@@ -1,5 +1,11 @@
-﻿using System.Windows;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace WearPartsControl.UserControls
@@ -9,51 +15,210 @@ namespace WearPartsControl.UserControls
     /// </summary>
     public partial class UserTabControl : UserControl
     {
-        private readonly Brush _primaryBrush;
+        private readonly DependencyPropertyDescriptor _tabIndexDescriptor;
+        private bool _isLoaded;
+        private bool _isUpdatingSelection;
+        private ToggleButton? _selectedButton;
 
         public UserTabControl()
         {
-            _primaryBrush = (Brush)this.FindResource("PrimaryBrush");
             InitializeComponent();
+
+            _tabIndexDescriptor = DependencyPropertyDescriptor.FromProperty(Control.TabIndexProperty, typeof(UserTabControl));
+            _tabIndexDescriptor.AddValueChanged(this, OnTabIndexChanged);
+
+            Loaded += OnLoaded;
+            Unloaded += OnUnloaded;
         }
 
         public IEnumerable<string> Headers
         {
-            get { return (IEnumerable<string>)GetValue(HeadersProperty); }
-            set { SetValue(HeadersProperty, value); }
+            get => (IEnumerable<string>)GetValue(HeadersProperty);
+            set => SetValue(HeadersProperty, value);
         }
 
-        // Using a DependencyProperty as the backing store for ItemsSource. This enables animation, styling, binding, etc...
         public static readonly DependencyProperty HeadersProperty =
-            DependencyProperty.Register("Headers", typeof(IEnumerable<string>), typeof(UserTabControl), new PropertyMetadata(new List<string>()));
+            DependencyProperty.Register(nameof(Headers), typeof(IEnumerable<string>), typeof(UserTabControl), new PropertyMetadata(new List<string>()));
 
-        private Border? _selectedBorder = null;
-
-        private void Grid_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        public ICommand? Command
         {
-            if (sender is Border border)
+            get => (ICommand?)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(UserTabControl), new PropertyMetadata(null));
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _isLoaded = true;
+            ApplySelection(TabIndex, invokeCommand: true);
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            _isLoaded = false;
+        }
+
+        private void OnTabIndexChanged(object? sender, EventArgs e)
+        {
+            ApplySelection(TabIndex, invokeCommand: _isLoaded);
+        }
+
+        private void TabItemButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (_isUpdatingSelection || sender is not ToggleButton button)
             {
-                if (border == _selectedBorder)
-                {
-                    return;
-                }
-
-                if (border != _selectedBorder)
-                {
-                    border.Background = _primaryBrush;
-                    var textBlock = (TextBlock)border.FindName("TextContent");
-                    textBlock.Foreground = Brushes.White;
-                }
-
-                if (_selectedBorder != null)
-                {
-                    _selectedBorder.Background = Brushes.AliceBlue;
-                    var textBlock = (TextBlock)_selectedBorder.FindName("TextContent");
-                    textBlock.Foreground = Brushes.Black;
-                }
-
-                _selectedBorder = border;
+                return;
             }
+
+            var index = GetItemIndex(button);
+            if (index < 0)
+            {
+                return;
+            }
+
+            if (TabIndex != index)
+            {
+                TabIndex = index;
+            }
+            else if (_isLoaded)
+            {
+                ExecuteCommand(GetHeader(index));
+            }
+        }
+
+        private void TabItemButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (_isUpdatingSelection || sender is not ToggleButton button)
+            {
+                return;
+            }
+
+            if (ReferenceEquals(button, _selectedButton))
+            {
+                _isUpdatingSelection = true;
+                button.IsChecked = true;
+                _isUpdatingSelection = false;
+            }
+        }
+
+        private void ApplySelection(int index, bool invokeCommand)
+        {
+            var headers = Headers?.ToList() ?? new List<string>();
+            if (headers.Count == 0)
+            {
+                return;
+            }
+
+            var normalizedIndex = NormalizeIndex(index, headers.Count);
+            var selectedButton = FindButtonByIndex(normalizedIndex);
+            if (selectedButton == null)
+            {
+                return;
+            }
+
+            _isUpdatingSelection = true;
+            try
+            {
+                if (_selectedButton != null && !ReferenceEquals(_selectedButton, selectedButton))
+                {
+                    _selectedButton.IsChecked = false;
+                }
+
+                selectedButton.IsChecked = true;
+                _selectedButton = selectedButton;
+            }
+            finally
+            {
+                _isUpdatingSelection = false;
+            }
+
+            if (invokeCommand)
+            {
+                ExecuteCommand(headers[normalizedIndex]);
+            }
+        }
+
+        private void ExecuteCommand(string header)
+        {
+            if (Command is null)
+            {
+                return;
+            }
+
+            if (Command.CanExecute(header))
+            {
+                Command.Execute(header);
+            }
+        }
+
+        private int NormalizeIndex(int index, int count)
+        {
+            if (count <= 0)
+            {
+                return -1;
+            }
+
+            if (index < 0)
+            {
+                return 0;
+            }
+
+            if (index >= count)
+            {
+                return count - 1;
+            }
+
+            return index;
+        }
+
+        private int GetItemIndex(DependencyObject element)
+        {
+            var container = ItemsControl.ContainerFromElement(TabItemsControl, element);
+            if (container is null)
+            {
+                return -1;
+            }
+
+            return TabItemsControl.ItemContainerGenerator.IndexFromContainer(container);
+        }
+
+        private ToggleButton? FindButtonByIndex(int index)
+        {
+            var container = TabItemsControl.ItemContainerGenerator.ContainerFromIndex(index) as DependencyObject;
+            if (container is null)
+            {
+                return null;
+            }
+
+            return FindDescendant<ToggleButton>(container);
+        }
+
+        private string GetHeader(int index)
+        {
+            return Headers.ElementAt(index);
+        }
+
+        private static T? FindDescendant<T>(DependencyObject root) where T : DependencyObject
+        {
+            var childCount = VisualTreeHelper.GetChildrenCount(root);
+            for (var i = 0; i < childCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(root, i);
+                if (child is T match)
+                {
+                    return match;
+                }
+
+                var descendant = FindDescendant<T>(child);
+                if (descendant != null)
+                {
+                    return descendant;
+                }
+            }
+
+            return null;
         }
     }
 }
--- src/WearPartsControl/ViewModels/MainWindowViewModel.cs
+++ src/WearPartsControl/ViewModels/MainWindowViewModel.cs
@@ -1,58 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 using WearPartsControl.ApplicationServices.Localization;
 
 namespace WearPartsControl.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
-        public string Title { get; set; }
+        private int _selectedTabIndex;
+        private string? _selectedTabHeader;
 
         public MainWindowViewModel(ILocalizationService localizationService)
         {
             Title = localizationService["MainWindow.Title"];
+            TabChangedCommand = new RelayCommand<string?>(OnTabChanged);
+            _selectedTabIndex = 0;
         }
+
+        public string Title { get; set; }
 
         public IEnumerable<string> Values { get; set; } = new List<string>()
         {
             "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "易损件更换",
-            "设备基础信息"
+            "设备基础信息",
+            "易损件管理",
+            "易损件更换历史",
+            "用户配置"
         };
+
+        public int SelectedTabIndex
+        {
+            get => _selectedTabIndex;
+            set
+            {
+                if (_selectedTabIndex == value)
+                {
+                    return;
+                }
+
+                _selectedTabIndex = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string? SelectedTabHeader
+        {
+            get => _selectedTabHeader;
+            private set
+            {
+                if (_selectedTabHeader == value)
+                {
+                    return;
+                }
+
+                _selectedTabHeader = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand TabChangedCommand { get; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnTabChanged(string? header)
+        {
+            SelectedTabHeader = header;
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
