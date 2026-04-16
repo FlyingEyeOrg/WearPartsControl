bd646bbb56346c6e86d4ce65fdd2389cea4a2709
UnknowName <13604461032@163.com>
2026-04-16 10:00:40 +0800

    删除不需要的 db 封装。

--- src/WearPartsControl/Infrastructure/Database/IDatabaseInitializer.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace WearPartsControl.Infrastructure.Database;
-
-public interface IDatabaseInitializer
-{
-    Task InitializeAsync(CancellationToken cancellationToken = default);
-}
